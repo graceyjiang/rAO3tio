eb4ebf220a61a0c1f79cbc06f0206114e1c7e557
DPHAM2020 <dpham2020@berkeley.edu>
2025-05-05 21:57:58 -0700

    attempting to switch to react, first commit

---
+++ rA03tio/src/Home.jsx
@@ -0,0 +1,37 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./Style.css";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+  const navigate = useNavigate();
+
+  const goToSearch = () => {
+    navigate("/search");
+  };
+
+  return (
+    <>
+      <div class="container">
+        <h1>
+          <span id="frontpage1">rAO3tio</span>
+        </h1>
+        <h2>
+          <span id="frontpage2">
+            {" "}
+            An advanced way to filter through works :){" "}
+          </span>{" "}
+        </h2>
+        <a href="search">
+          {" "}
+          <button onClick={goToSearch}>Let's go!</button>{" "}
+        </a>
+
+        <button id="toggleDarkMode">Dark Mode</button>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ rA03tio/src/Search.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import "./Style.css";
+
+function Search() {
+  return (
+    <div class="container2">
+      <h2>
+        <span id="whattag">What tag are you looking for?</span>
+      </h2>
+    </div>
+  );
+}
+
+// export default Search;
