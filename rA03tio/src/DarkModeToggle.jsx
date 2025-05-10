1ea256da2ae8553f1e0dd4b021dd34b83d1f96f2
graceyjiang <gracie.y.jiang@gmail.com>
2025-05-09 18:19:15 -0700

    dark mode + changed some styling

---
+++ rA03tio/src/DarkModeToggle.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import React from "react";
+
+function DarkModeToggle() {
+  // Check localStorage and set the initial state based on that
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("darkMode") === "enabled";
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode"); // adds dark-mode class to body
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]); // Dependency array: React will re-run the effect when darkMode changes
+
+  // Function to toggle dark mode
+  const toggleDarkMode = () => {
+    const isEnabled = !darkMode;
+    setDarkMode(isEnabled);
+
+    // Save the user's preference in localStorage
+    if (isEnabled) {
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      localStorage.setItem("darkMode", "disabled");
+    }
+  };
+
+  return (
+    <button id="toggleDarkMode" onClick={toggleDarkMode}>
+      Dark Mode
+      {/* {darkMode ? "Disable Dark Mode" : "Enable Dark Mode"} */}
+    </button>
+  );
+}
+
+export default DarkModeToggle;
--- rA03tio/src/Home.jsx
+++ rA03tio/src/Home.jsx
@@ -3,15 +3,18 @@
 // import viteLogo from '/vite.svg'
 import "./Style.css";
 import { useNavigate } from "react-router-dom";
+import DarkModeToggle from "./DarkModeToggle";
+import React from "react";
 
 function Home() {
+  const navigate = useNavigate();
   const goToSearch = () => {
-    useNavigate("/search");
+    navigate("/search");
   };
 
   return (
     <>
-      <div class="container">
+      <div className="container">
         <h1>
           <span id="frontpage1">rAO3tio</span>
         </h1>
@@ -21,12 +24,8 @@ function Home() {
             An advanced way to filter through works :){" "}
           </span>{" "}
         </h2>
-        <a href="search">
-          {" "}
-          <button onClick={goToSearch}>Let's go!</button>{" "}
-        </a>
-
-        <button id="toggleDarkMode">Dark Mode</button>
+        <button onClick={goToSearch}>Let's go!</button>
+        <DarkModeToggle />
       </div>
     </>
   );
